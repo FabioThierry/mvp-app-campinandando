5b33ba354f84f22851e8a1c6e6e20d9b5989592a
FabioThierry <fabiodomingues.08@gmail.com>
2026-04-01 22:36:40 -0300

    Remove PWA plugin from dependencies and configuration in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: { display: "standalone" },
-    }),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   manifest: { display: "standalone" },
+    // }),
   ],
   resolve: {
     alias: {
